2dab9336d2f7aa6bd1a64bd3016a619d4883f767
Jaskarn <jaskarn15@gmail.com>
2022-11-24 22:34:06 +0000

    add qeury for to retreive reservations for a user

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -98,8 +98,27 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+  return pool
+  .query(`
+    SELECT reservations.id AS id, properties.*, reservations.start_date AS start_date, reservations.end_date AS end_date, AVG(property_reviews.rating) AS rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY reservations.id, properties.id
+    ORDER BY start_date
+    LIMIT $2
+  `, 
+  [guest_id, limit]
+  )
+  .then(res => {
+    console.log(res.rows);
+    return res.rows;
+  })
+  .catch(err => {
+    console.log(err.message);
+  });
+};
 exports.getAllReservations = getAllReservations;
 
 /// Properties
